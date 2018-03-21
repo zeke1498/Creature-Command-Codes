859edb0273fc91fc489d82901295d17a1b56c705
zeke1498 <37006052+zeke1498@users.noreply.github.com>
2018-03-21 17:27:37 -0500

    Add files via upload
    Built out Character intents from your template in the index

---
+++ ChooseCharIntents.js
@@ -0,0 +1,47 @@
+'klaki_chosen' : function(){
+    // make a differnt intent for each of the characters following this logic!
+    if(pselction == 1){
+       var firstPlayerChar = "klaki"; 
+       pselction = 2;
+       this.emit(':ask',"Player one you chose Klaki ,Player two please chose a character!");
+    }else{
+        var secondPlayerChar = "klaki";
+        this.emit('attack_handel');
+    }
+},
+
+'varu_chosen' : function(){
+    // make a differnt intent for each of the characters following this logic!
+    if(pselction == 1){
+       var firstPlayerChar = "varu"; 
+       pselction = 2;
+       this.emit(':ask',"Player one you chose Varu ,Player two please chose a character!");
+    }else{
+        var secondPlayerChar = "varu";
+        this.emit('attack_handel');
+    }
+},
+
+'momolt_chosen' : function(){
+    // make a differnt intent for each of the characters following this logic!
+    if(pselction == 1){
+       var firstPlayerChar = "momolt"; 
+       pselction = 2;
+       this.emit(':ask',"Player one you chose Momolt ,Player two please chose a character!");
+    }else{
+        var secondPlayerChar = "momolt";
+        this.emit('attack_handel');
+    }
+},
+
+'babool_chosen' : function(){
+    // make a differnt intent for each of the characters following this logic!
+    if(pselction == 1){
+       var firstPlayerChar = "babool"; 
+       pselction = 2;
+       this.emit(':ask',"Player one you chose Babool ,Player two please chose a character!");
+    }else{
+        var secondPlayerChar = "babool";
+        this.emit('attack_handel');
+    }
+},
