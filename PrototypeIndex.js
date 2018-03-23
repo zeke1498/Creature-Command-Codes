f1607a301b2c327f3c4e28fb383ddfd7bd1bd281
zeke1498 <37006052+zeke1498@users.noreply.github.com>
2018-03-22 22:37:40 -0500

    Update PrototypeIndex.js

--- PrototypeIndex.js
+++ PrototypeIndex.js
@@ -886,37 +886,57 @@ const handlers = {
     this.response.playVideo(videoSource, metadata);
         this.emit(':responseReady');
     },
-    'clacky_chosen' : function(){
-        // make a differnt intent for each of the characters following this logic!
-        if(pselction == 1){
-            firstPlayerChar = 4; 
-            pselction = 2;
-           this.emit(':ask',"Player one you chose clacky ,Player two please choose a character!");
-        }else{
-            secondPlayerChar = 4;
-            turn = 1;
-             console.log(turn);
-             console.log(firstPlayerChar);
-            this.emit(':ask',"Player two you chose clacky ,Player one choose an attack");
-            //this.emit('attack_handel');
-        }
-        
-    },
-        'varu_chosen' : function(){
-        // make a differnt intent for each of the characters following this logic!
-        if(pselction == 1){
-            firstPlayerChar = 1; 
-           pselction = 2;
-           this.emit(':ask',"Player one you chose varu ,Player two please choose a character!");
-        }else{
-            secondPlayerChar = 1;
-            console.log(turn);
-            console.log(firstPlayerChar);
-            this.emit(':ask',"Player two you chose varu ,Player one choose an attack");
-            //this.emit('attack_handel');
-        }
-        
-    },
+'clacky_chosen' : function(){
+    // make a differnt intent for each of the characters following this logic!
+    if(pselction == 1){
+       firstPlayerChar = klaki; 
+       pselction = 2;
+       this.emit(':ask',"Player one you chose clacky ,Player two please choose a character!");
+    }else{
+        secondPlayerChar = klaki;
+        turn = 1;
+        this.emit(':ask',"Player two you chose clacky ,Player one choose an attack");
+    }
+},
+
+'varu_chosen' : function(){
+    // make a differnt intent for each of the characters following this logic!
+    if(pselction == 1){
+       firstPlayerChar = varu; 
+       pselction = 2;
+       this.emit(':ask',"Player one you chose Varu ,Player two please choose a character!");
+    }else{
+        secondPlayerChar = varu;
+        turn = 1;
+        this.emit(':ask',"Player two you chose Varu ,Player one choose an attack");
+    }
+},
+
+'momolt_chosen' : function(){
+    // make a differnt intent for each of the characters following this logic!
+    if(pselction == 1){
+       firstPlayerChar = momolt; 
+       pselction = 2;
+       this.emit(':ask',"Player one you chose Momolt ,Player two please choose a character!");
+    }else{
+        secondPlayerChar = momolt;
+        turn = 1;
+        this.emit(':ask',"Player two you chose Momolt ,Player one choose an attack");
+    }
+},
+
+'babool_chosen' : function(){
+    // make a differnt intent for each of the characters following this logic!
+    if(pselction == 1){
+       firstPlayerChar = babool; 
+       pselction = 2;
+       this.emit(':ask',"Player one you chose Babool ,Player two please choose a character!");
+    }else{
+        secondPlayerChar = babool;
+        turn = 1;
+        this.emit(':ask',"Player two you chose Babool ,Player one choose an attack");
+    }
+},
     'LaunchRequest': function () {
         this.emit(':ask', "Player one, Please select a character!", this.attributes.repromptSpeech);
     },
