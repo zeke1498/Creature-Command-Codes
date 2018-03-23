5dffa1dfa8971e279f4d78126d021ee75ff19b4c
Pizzaslice3 <lcpiazza626@gmail.com>
2018-03-23 00:28:53 -0500

    Update ChooseCharIntents.js

--- ChooseCharIntents.js
+++ ChooseCharIntents.js
@@ -1,28 +1,42 @@
-'klaki_chosen' : function(){
-    // make a differnt intent for each of the characters following this logic!
-    if(pselction == 1){
-       firstPlayerChar = klaki; 
-       pselction = 2;
-       this.emit(':ask',"Player one you chose clacky ,Player two please choose a character!");
-    }else{
-        secondPlayerChar = klaki;
-        turn = 1;
-        this.emit(':ask',"Player two you chose clacky ,Player one choose an attack");
-    }
-},
+   'clacky_chosen' : function(){
+        // make a differnt intent for each of the characters following this logic!
+        if(pselection == 1){
+            firstPlayerChar = 4; 
+            pselection = 2;
+           this.emit(':ask',"Player one you chose clacky ,Player two please choose a character!");
+        }else{
+            secondPlayerChar = 4;
+            turn = 1;
+                const cardTitle = 'CURRENT HP';
+                const cardContent = 'Player1hp: '+pOneHp+'/50  PLayer2hp'+pTwoHp+'/50';
+                const imageObj = {
+                       smallImageUrl: 'https://s3.amazonaws.com/creaturecommand/KlakiChooseAttack.jpg',
+                       largeImageUrl: 'https://s3.amazonaws.com/creaturecommand/KlakiChooseAttack.jpg'
+                    };
+            this.emit(':askWithCard',"Player two you chose clacky ,Player one choose an attack", cardTitle, cardContent, imageObj);
 
-'varu_chosen' : function(){
-    // make a differnt intent for each of the characters following this logic!
-    if(pselction == 1){
-       firstPlayerChar = varu; 
-       pselction = 2;
-       this.emit(':ask',"Player one you chose Varu ,Player two please choose a character!");
-    }else{
-        secondPlayerChar = varu;
-        turn = 1;
-        this.emit(':ask',"Player two you chose Varu ,Player one choose an attack");
-    }
-},
+
+            //this.emit('attack_handel');
+        }
+        
+    },
+        'varu_chosen' : function(){
+        // make a differnt intent for each of the characters following this logic!
+        if(pselection == 1){
+            firstPlayerChar = 1; 
+           pselection = 2;
+           this.emit(':ask',"Player one you chose varu ,Player two please choose a character!");
+        }else{
+            secondPlayerChar = 1;
+           const cardTitle = 'CURRENT HP';
+                const cardContent = 'Player1hp: '+pOneHp+'/50  PLayer2hp'+pTwoHp+'/50';
+                const imageObj = {
+                      smallImageUrl: 'https://s3.amazonaws.com/creaturecommand/VaruChooseAttack.jpg',
+                      largeImageUrl: 'https://s3.amazonaws.com/creaturecommand/VaruChooseAttack.jpg'
+                    };
+            this.emit(':askWithCard',"Player two you chose varu ,Player one choose an attack", cardTitle, cardContent, imageObj);
+      }  
+    },
 
 'momolt_chosen' : function(){
     // make a differnt intent for each of the characters following this logic!
